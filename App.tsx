edbc2601058dff2f66e049469e3b3fa11eb5463e
Maurilio <marquesmaurili049@gmailcom>
2025-12-15 20:11:17 -0300

    feature: add Tela de impar e Par

--- App.tsx
+++ App.tsx
@@ -4,12 +4,15 @@ import { StatusBar } from 'expo-status-bar';
 import { LoginScreen } from '@/screens/login';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Soma } from '@/screens/soma';
-
+import { Telasoma } from '@/screens/Telasoma';
+import { TelaContador } from '@/screens/TelaContador';
+import { Media } from '@/screens/media';
+import { ParImpar } from '@/screens/Impar_Par';
 export default function App() {
   return (
     <>
       <SafeAreaView className="flex-1">
-       <Soma />
+       <ParImpar/>
       </SafeAreaView>
 
       <StatusBar style="auto" />
